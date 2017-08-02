a8a6398499e5bba96ccf6f062c7975157433550d
Yogesh Choudhary <yogesum@gmail.com>
2017-08-02 19:31:51 +0530

    Finished text_learning and preprocessing

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,35 +41,45 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        #temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            print(path)
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            stemmed = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            text = re.sub(r'sara|shackleton|chris|germani', '', stemmed)
 
             ### append the text to word_data
+            word_data.append(text)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            from_data.append(1 if name == 'chris' else 0)
 
             email.close()
 
-print "emails processed"
+print("emails processed")
 from_sara.close()
 from_chris.close()
+print('word_data[152]: ', word_data[152])
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+pickle.dump( word_data, open("your_word_data.pkl", "wb") )
+pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
 
 
 
 
 
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+vectorizer = TfidfVectorizer(stop_words='english')
+vectorizer.fit_transform(word_data)
 
+feature_words = vectorizer.get_feature_names()
+print('Unique words in TfIdf: ', len(feature_words))
+print('34597th word in TfIdf: ', feature_words[34597])
