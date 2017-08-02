e4ecdf93e523aa1250c4c9e6efea5072686adb1e
Yogesh Choudhary <yogesum@gmail.com>
2017-08-02 23:49:06 +0530

    Finished feature selection outlier removal for signature

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -10,8 +10,8 @@ numpy.random.seed(42)
 ### mini-project.
 words_file = "../text_learning/your_word_data.pkl" 
 authors_file = "../text_learning/your_email_authors.pkl"
-word_data = pickle.load( open(words_file, "r"))
-authors = pickle.load( open(authors_file, "r") )
+word_data = pickle.load( open(words_file, "rb"))
+authors = pickle.load( open(authors_file, "rb") )
 
 
 
@@ -19,8 +19,8 @@ authors = pickle.load( open(authors_file, "r") )
 ### remainder go into training)
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+from sklearn import model_selection
+features_train, features_test, labels_train, labels_test = model_selection.train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
@@ -38,6 +38,17 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
 
+pos, max_imp = 0, 0
+for idx, weight in enumerate(clf.feature_importances_):
+  if weight > max_imp:
+    pos, max_imp = idx, weight
 
+print('Accuracy for overfit DTC: ', accuracy_score(labels_test, clf.predict(features_test)))
+print('Highest feature importance: ', (pos, max_imp))
+print('Most powerful word: ', vectorizer.get_feature_names()[pos])
