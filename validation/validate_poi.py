ef5c3f35777b82699959e27764112222d178ffc0
Yogesh Choudhary <yogesum@gmail.com>
2017-08-07 16:21:12 +0530

    Finished validation except last quiz

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -15,7 +15,7 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -28,5 +28,17 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
+clf = DecisionTreeClassifier()
+clf.fit(features, labels)
 
+print('Accuracy pre_split %0.8f' % (accuracy_score(labels, clf.predict(features))))
+
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.30, random_state=42)
+clf2 = DecisionTreeClassifier()
+clf2.fit(X_train, y_train)
+
+print('Accuracy post_split %0.8f' % (accuracy_score(y_test, clf2.predict(X_test))))
